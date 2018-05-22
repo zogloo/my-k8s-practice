b37dd882808a3741e503162ed4bb2d0e1a0a02bd
zogloo <chunglou@163.com>
2018-05-22 11:22:43 +0800

    commit

---
+++ rolling-update-test/main.go
@@ -0,0 +1,19 @@
+﻿package main
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+func sayhello(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "This is version 1.") //
+}
+
+func main() {
+    http.HandleFunc("/", sayhello) //
+    log.Println("This is version 1.")
+    err := http.ListenAndServe(":9090", nil) //
+    if err != nil {
+        log.Fatal("ListenAndServe: ", err)
+    }
+}
